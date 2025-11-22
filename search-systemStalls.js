4111f6c9d0b275d89edb739ec42d80a87b6b5366
nat-cee <nathaliecornella.10@gmail.com>
2025-11-22 15:50:35 +0800

    v 5.00

--- search-systemStalls.js
+++ search-systemStalls.js
@@ -101,3 +101,12 @@ if (window.location.pathname.includes("stalls.html")) {
 
     runSearch(term);
 }
+
+    function showSidebar(){
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'flex'
+    }
+    function hideSidebar(){
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'none'
+    }
